4c76ead768c715cade906a8f0107bac35f749904
ga3riel <kruszkagabrys@gmail.com>
2025-07-19 15:24:13 +0200

    Initial commit

---
+++ app/_layout.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import React from "react";
+import "./global.css";
+
+const RootLayout = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>RootLayout</Text>
+    </View>
+  );
+};
+
+export default RootLayout;

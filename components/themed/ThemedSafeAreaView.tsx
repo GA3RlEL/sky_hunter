06b3b1be163bc3fc39ef8885a3b5483e11d9d973
jagorczyk <mrigork2@gmail.com>
2025-07-20 20:34:01 +0200

    added themedsafeareaview funcionality

---
+++ components/themed/ThemedSafeAreaView.tsx
@@ -0,0 +1,28 @@
+ 
+import React from "react";
+import { SafeAreaView, StyleProp, useColorScheme, ViewStyle } from "react-native";
+import { Colors } from "../../constant/Colors";
+import { SafeAreaViewProps } from "react-native-safe-area-context";
+
+interface ThemedSafeAreaViewProps extends SafeAreaViewProps {
+    children?: React.ReactNode;
+    style?: StyleProp<ViewStyle>;
+    className?: string;
+}
+
+export default function ThemedSafeAreaView({
+    children,
+    style,
+    className,
+    ...props
+}: ThemedSafeAreaViewProps) {
+  const scheme = useColorScheme();
+  const theme = Colors[scheme!] ?? Colors.light;
+
+    return (
+        <SafeAreaView style={[style, {backgroundColor: theme.background }]} className={className} {...props}>
+            {children}
+        </SafeAreaView>
+    );
+}
+

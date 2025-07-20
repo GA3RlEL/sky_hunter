10d5ab4cfd22839cb2719b3a311633df31580549
jagorczyk <mrigork2@gmail.com>
2025-07-20 13:55:07 +0200

    added themed colors

---
+++ constant/Colors.ts
@@ -0,0 +1,20 @@
+export const Colors = {
+    light: {
+        background: '#F6F7F8',
+        
+        primary: '#FFFFFF',
+        secondary: '#F6F7F8',
+
+        text: '#000000',
+        secondaryText: '#6B7280'
+    },
+    dark: {
+        background: '#1A1D21',
+
+        primary: '#111827',
+        secondary: '#3B4252',
+
+        text: '#FFFFFF',
+        secondaryText: '#94A3B8'
+    }
+}

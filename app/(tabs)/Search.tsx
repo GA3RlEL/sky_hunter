0057771fa39fbaf02a7c8f2820e7b73e34acd88d
ga3riel <kruszkagabrys@gmail.com>
2025-07-20 22:48:22 +0200

    Added flightInputs, LabeledInput and LabeledInputDate with new npm package

--- app/(tabs)/Search.tsx
+++ app/(tabs)/Search.tsx
@@ -1,20 +1,32 @@
-import { View, Text } from 'react-native'
-import React, { useState } from 'react'
-import ThemedButton from '../../components/themed/ThemedButton'
-import { Ionicons } from '@expo/vector-icons';
+import { View } from "react-native";
+import React, { useState } from "react";
+import ThemedButton from "../../components/themed/ThemedButton";
+import ThemedSafeAreaView from "../../components/themed/ThemedSafeAreaView";
+import ThemedText from "../../components/themed/ThemedText";
+import FlightInputs from "../../components/search/FlightInputs";
+import ThemedView from "../../components/themed/ThemedView";
 
 const Search = () => {
   const [focused, setFocused] = useState(false);
 
   return (
-    <View className='flex-1 items-center justify-center'>
-      <ThemedButton text='Search Flights' onPress={() => setFocused(!focused)} isFocused={focused} className='p-4 rounded-lg flex flex-row items-center justify-center'
+    <ThemedSafeAreaView className="flex-1 gap-6">
+      <ThemedView className="gap-3 p-6">
+        <ThemedText className="text-4xl font-bold">SkyBook</ThemedText>
+        <ThemedText>Find your perfect flight</ThemedText>
+      </ThemedView>
+      <View className="w-[90%] mx-auto">
+        <FlightInputs />
+      </View>
+      <ThemedButton
+        text="Search Flights"
+        onPress={() => setFocused(!focused)}
+        isFocused={focused}
+        className="p-4 rounded-lg flex flex-row items-center justify-center"
         icon={"search"}
-        >
+      ></ThemedButton>
+    </ThemedSafeAreaView>
+  );
+};
 
-      </ThemedButton>
-    </View>
-  )
-}
-
-export default Search
+export default Search;
--- constant/Colors.ts
+++ constant/Colors.ts
@@ -1,34 +1,34 @@
 export const Colors = {
-    light: {
-        background: '#F6F7F8',
-        
-        primary: '#FFFFFF',
-        secondary: '#F6F7F8',
+  light: {
+    background: "#F6F7F8",
 
-        text: '#000000',
-        secondaryText: '#6B7280',
+    primary: "#FFFFFF",
+    secondary: "#F6F7F8",
 
-        buttonColor: '#FFFFFF',
-        buttonFocusedColor: '#0077B6',
-        buttonTextColor: '#6B7280',
-        buttonFocusedTextColor: '#FFFFFF',
-        buttonBorderColor: '#6B7280',
-        buttonFocusedBorderColor: '#0077B6',
-    },
-    dark: {
-        background: '#1A1D21',
+    text: "#000000",
+    secondaryText: "#6B7280",
 
-        primary: '#111827',
-        secondary: '#3B4252',
+    buttonColor: "#FFFFFF",
+    buttonFocusedColor: "#0077B6",
+    buttonTextColor: "#6B7280",
+    buttonFocusedTextColor: "#FFFFFF",
+    buttonBorderColor: "#6B7280",
+    buttonFocusedBorderColor: "#0077B6",
+  },
+  dark: {
+    background: "#1A1D21",
 
-        text: '#FFFFFF',
-        secondaryText: '#94A3B8',
+    primary: "#252A34",
+    secondary: "#3B4252",
 
-        buttonColor: '#3B4252',
-        buttonFocusedColor: '#0077B6',
-        buttonTextColor: '#8E9CB1',
-        buttonFocusedTextColor: '#FFFFFF',
-        buttonBorderColor: '#3B4252',
-        buttonFocusedBorderColor: '#0077B6',
-    }
-}
+    text: "#FFFFFF",
+    secondaryText: "#94A3B8",
+
+    buttonColor: "#3B4252",
+    buttonFocusedColor: "#0077B6",
+    buttonTextColor: "#8E9CB1",
+    buttonFocusedTextColor: "#FFFFFF",
+    buttonBorderColor: "#3B4252",
+    buttonFocusedBorderColor: "#0077B6",
+  },
+};
